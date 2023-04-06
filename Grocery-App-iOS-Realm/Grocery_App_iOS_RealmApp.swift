1b014376ea34bfac030b0e6c2a59b7c124c49ff3
Junaed <junaed1995@gmail.com>
2023-04-06 17:03:41 +0600

    Initial Commit

---
+++ Grocery-App-iOS-Realm/Grocery_App_iOS_RealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grocery_App_iOS_RealmApp.swift
+//  Grocery-App-iOS-Realm
+//
+//  Created by Junaed Muhammad Chowdhury on 2/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Grocery_App_iOS_RealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
